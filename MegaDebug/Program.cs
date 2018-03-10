7a9db4cedf08e6a884dca17b5ebcf418f00bec42
Cameron Armstrong <cameronscottarmstrong@gmail.com>
2018-03-10 21:33:19 +0800

    Added serial read code

--- MegaDebug/Program.cs
+++ MegaDebug/Program.cs
@@ -1,15 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.IO.Ports;
 
-namespace MegaDebug
+
+namespace usb_test
 {
     class Program
     {
         static void Main(string[] args)
         {
+            SerialPort port = null;
+
+            try
+            {
+                string[] ports = SerialPort.GetPortNames();
+
+                Console.WriteLine("Please enter port name");
+                Console.WriteLine("Available ports:");
+                for (int i = 0; i < ports.Length; i++)
+                {
+                    Console.WriteLine(ports[i]);
+                }
+
+                port = new SerialPort(Console.ReadLine());
+                port.Open();
+
+                Console.WriteLine("Connected to " + port.PortName);
+
+                for (; ; )
+                {
+                    Console.Write("" + (char)port.ReadByte());
+                }
+
+            }
+            catch (Exception x)
+            {
+                Console.WriteLine("ERROR: " + x.Message);
+            }
         }
     }
 }
