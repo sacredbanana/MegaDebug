c743edd5fb2cab6cd3ebb0541d51d46d4a86e0ec
Cameron Armstrong <cameronscottarmstrong@gmail.com>
2018-03-12 00:55:12 +0800

    Added Mega Drive component and added data stream and registry viewer

---
+++ MegaDebug/Debug.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.IO.Ports;
+
+namespace MegaDebug
+{
+    public partial class Program
+    {
+        private static void Debug(SerialPort port)
+        {
+            Console.WriteLine("Select operation:");
+            Console.WriteLine("1. Motorola 68000 Registry Contents");
+            RegistryContents(port);
+        }
+
+        private static void RegistryContents(SerialPort port)
+        {
+            bool debugDataHeaderCaptured = false;
+            while (!debugDataHeaderCaptured)
+            {
+                if ((char)port.ReadByte() == 'D')
+                {
+                    if ((char)port.ReadByte() == 'B')
+                    {
+                        if ((char)port.ReadByte() == 'G')
+                        {
+                            debugDataHeaderCaptured = true;
+                        }
+                    }
+                }
+            }
+
+            byte[] byteArray = new byte[4];
+
+            byteArray[3] = (byte)port.ReadByte();
+            byteArray[2] = (byte)port.ReadByte();
+            byteArray[1] = (byte)port.ReadByte();
+            byteArray[0] = (byte)port.ReadByte();
+
+            if (BitConverter.IsLittleEndian) Array.Reverse(byteArray);
+            UInt32 longData = BitConverter.ToUInt32(byteArray, 0);
+            Console.WriteLine($"D0: {longData.ToString("X")}");
+
+            byteArray[3] = (byte)port.ReadByte();
+            byteArray[2] = (byte)port.ReadByte();
+            byteArray[1] = (byte)port.ReadByte();
+            byteArray[0] = (byte)port.ReadByte();
+
+            if (BitConverter.IsLittleEndian) Array.Reverse(byteArray);
+            longData = BitConverter.ToUInt32(byteArray, 0);
+            Console.WriteLine($"D1: {longData.ToString("X")}");
+
+            byteArray[3] = (byte)port.ReadByte();
+            byteArray[2] = (byte)port.ReadByte();
+            byteArray[1] = (byte)port.ReadByte();
+            byteArray[0] = (byte)port.ReadByte();
+
+            if (BitConverter.IsLittleEndian) Array.Reverse(byteArray);
+            longData = BitConverter.ToUInt32(byteArray, 0);
+            Console.WriteLine($"D2: {longData.ToString("X")}");
+
+            byteArray[3] = (byte)port.ReadByte();
+            byteArray[2] = (byte)port.ReadByte();
+            byteArray[1] = (byte)port.ReadByte();
+            byteArray[0] = (byte)port.ReadByte();
+
+            if (BitConverter.IsLittleEndian) Array.Reverse(byteArray);
+            longData = BitConverter.ToUInt32(byteArray, 0);
+            Console.WriteLine($"D3: {longData.ToString("X")}");
+
+            byteArray[3] = (byte)port.ReadByte();
+            byteArray[2] = (byte)port.ReadByte();
+            byteArray[1] = (byte)port.ReadByte();
+            byteArray[0] = (byte)port.ReadByte();
+
+            if (BitConverter.IsLittleEndian) Array.Reverse(byteArray);
+            longData = BitConverter.ToUInt32(byteArray, 0);
+            Console.WriteLine($"D4: {longData.ToString("X")}");
+
+            byteArray[3] = (byte)port.ReadByte();
+            byteArray[2] = (byte)port.ReadByte();
+            byteArray[1] = (byte)port.ReadByte();
+            byteArray[0] = (byte)port.ReadByte();
+
+            if (BitConverter.IsLittleEndian) Array.Reverse(byteArray);
+            longData = BitConverter.ToUInt32(byteArray, 0);
+            Console.WriteLine($"D5: {longData.ToString("X")}");
+
+            byteArray[3] = (byte)port.ReadByte();
+            byteArray[2] = (byte)port.ReadByte();
+            byteArray[1] = (byte)port.ReadByte();
+            byteArray[0] = (byte)port.ReadByte();
+
+            if (BitConverter.IsLittleEndian) Array.Reverse(byteArray);
+            longData = BitConverter.ToUInt32(byteArray, 0);
+            Console.WriteLine($"D6: {longData.ToString("X")}");
+
+            byteArray[3] = (byte)port.ReadByte();
+            byteArray[2] = (byte)port.ReadByte();
+            byteArray[1] = (byte)port.ReadByte();
+            byteArray[0] = (byte)port.ReadByte();
+
+            if (BitConverter.IsLittleEndian) Array.Reverse(byteArray);
+            longData = BitConverter.ToUInt32(byteArray, 0);
+            Console.WriteLine($"D7: {longData.ToString("X")}");
+
+            byteArray[3] = (byte)port.ReadByte();
+            byteArray[2] = (byte)port.ReadByte();
+            byteArray[1] = (byte)port.ReadByte();
+            byteArray[0] = (byte)port.ReadByte();
+
+            if (BitConverter.IsLittleEndian) Array.Reverse(byteArray);
+            longData = BitConverter.ToUInt32(byteArray, 0);
+            Console.WriteLine($"A0: {longData.ToString("X")}");
+
+            byteArray[3] = (byte)port.ReadByte();
+            byteArray[2] = (byte)port.ReadByte();
+            byteArray[1] = (byte)port.ReadByte();
+            byteArray[0] = (byte)port.ReadByte();
+
+            if (BitConverter.IsLittleEndian) Array.Reverse(byteArray);
+            longData = BitConverter.ToUInt32(byteArray, 0);
+            Console.WriteLine($"A1: {longData.ToString("X")}");
+
+            byteArray[3] = (byte)port.ReadByte();
+            byteArray[2] = (byte)port.ReadByte();
+            byteArray[1] = (byte)port.ReadByte();
+            byteArray[0] = (byte)port.ReadByte();
+
+            if (BitConverter.IsLittleEndian) Array.Reverse(byteArray);
+            longData = BitConverter.ToUInt32(byteArray, 0);
+            Console.WriteLine($"A2: {longData.ToString("X")}");
+
+            byteArray[3] = (byte)port.ReadByte();
+            byteArray[2] = (byte)port.ReadByte();
+            byteArray[1] = (byte)port.ReadByte();
+            byteArray[0] = (byte)port.ReadByte();
+
+            if (BitConverter.IsLittleEndian) Array.Reverse(byteArray);
+            longData = BitConverter.ToUInt32(byteArray, 0);
+            Console.WriteLine($"A3: {longData.ToString("X")}");
+
+            byteArray[3] = (byte)port.ReadByte();
+            byteArray[2] = (byte)port.ReadByte();
+            byteArray[1] = (byte)port.ReadByte();
+            byteArray[0] = (byte)port.ReadByte();
+
+            if (BitConverter.IsLittleEndian) Array.Reverse(byteArray);
+            longData = BitConverter.ToUInt32(byteArray, 0);
+            Console.WriteLine($"A4: {longData.ToString("X")}");
+
+            byteArray[3] = (byte)port.ReadByte();
+            byteArray[2] = (byte)port.ReadByte();
+            byteArray[1] = (byte)port.ReadByte();
+            byteArray[0] = (byte)port.ReadByte();
+
+            if (BitConverter.IsLittleEndian) Array.Reverse(byteArray);
+            longData = BitConverter.ToUInt32(byteArray, 0);
+            Console.WriteLine($"A5: {longData.ToString("X")}");
+
+            byteArray[3] = (byte)port.ReadByte();
+            byteArray[2] = (byte)port.ReadByte();
+            byteArray[1] = (byte)port.ReadByte();
+            byteArray[0] = (byte)port.ReadByte();
+
+            if (BitConverter.IsLittleEndian) Array.Reverse(byteArray);
+            longData = BitConverter.ToUInt32(byteArray, 0);
+            Console.WriteLine($"A6: {longData.ToString("X")}");
+
+            byteArray[3] = (byte)port.ReadByte();
+            byteArray[2] = (byte)port.ReadByte();
+            byteArray[1] = (byte)port.ReadByte();
+            byteArray[0] = (byte)port.ReadByte();
+
+            if (BitConverter.IsLittleEndian) Array.Reverse(byteArray);
+            longData = BitConverter.ToUInt32(byteArray, 0);
+            Console.WriteLine($"A7: {longData.ToString("X")}");
+            Console.ReadKey();
+        }
+    }
+}
--- MegaDebug/Program.cs
+++ MegaDebug/Program.cs
@@ -3,12 +3,15 @@ using System.Text;
 using System.IO;
 using System.IO.Ports;
 
-
-namespace usb_test
+namespace MegaDebug
 {
-    class Program
+    public partial class Program
     {
-        static void Main(string[] args)
+        private const byte DATA_STREAM_MODE_ASCII = 1;
+        private const byte DATA_STREAM_MODE_DECIMAL = 2;
+        private const byte DATA_STREAM_MODE_HEXADECIMAL = 3;
+
+        private static void Main(string[] args)
         {
             SerialPort port = null;
 
@@ -16,27 +19,73 @@ namespace usb_test
             {
                 string[] ports = SerialPort.GetPortNames();
 
-                Console.WriteLine("Please enter port name");
+                Console.WriteLine("Please enter port name or push enter to use first one.");
                 Console.WriteLine("Available ports:");
                 for (int i = 0; i < ports.Length; i++)
                 {
                     Console.WriteLine(ports[i]);
                 }
+                string portSelection = Console.ReadLine();
+                if (portSelection.Equals(""))
+                    portSelection = ports[0];
 
-                port = new SerialPort(Console.ReadLine());
+                port = new SerialPort(portSelection);
                 port.Open();
 
                 Console.WriteLine("Connected to " + port.PortName);
 
-                for (; ; )
+                Console.WriteLine("Select operation:");
+                Console.WriteLine("1. Debug");
+                Console.WriteLine("2. Read data stream");
+
+                byte operation = Convert.ToByte(Console.ReadLine());
+
+                switch(operation)
                 {
-                    Console.Write("" + (char)port.ReadByte());
+                    case 1:
+                        Debug(port);
+                        break;
+                    case 2:
+                        Console.WriteLine("Select data interpretation:");
+                        Console.WriteLine("1. ASCII");
+                        Console.WriteLine("2. Decimal");
+                        Console.WriteLine("3. Hexadecimal");
+                        bool validInterpretationMode = false;
+                        byte mode = 0;
+                        while (!validInterpretationMode)
+                        {
+                            mode = Convert.ToByte(Console.ReadLine());
+                            if (mode == DATA_STREAM_MODE_ASCII || mode == DATA_STREAM_MODE_DECIMAL || mode == DATA_STREAM_MODE_HEXADECIMAL) validInterpretationMode = true;
+                        }
+
+                        ReadDataStream(port, mode);
+                        break;
                 }
 
             }
             catch (Exception x)
             {
                 Console.WriteLine("ERROR: " + x.Message);
+            }
+        }
+
+        private static void ReadDataStream(SerialPort port, byte mode)
+        {
+            while (true)
+            {
+                switch (mode)
+                {
+                    case DATA_STREAM_MODE_ASCII:
+                        Console.WriteLine((char)port.ReadByte());
+                        break;
+                    case DATA_STREAM_MODE_DECIMAL:
+                        Console.WriteLine(port.ReadByte());
+                        break;
+                    case DATA_STREAM_MODE_HEXADECIMAL:
+                        Console.WriteLine(port.ReadByte().ToString("X"));
+                        break;
+                }
+               
             }
         }
     }
